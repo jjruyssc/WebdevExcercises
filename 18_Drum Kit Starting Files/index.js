618351d23f3d1a3d2ab5c9ae0045a0ab9d90a48e
Justine <justine.ruysschaert@hotmail.com>
2024-04-08 18:03:52 +0200

    toevoegen eventlistener drumkit

--- 18_Drum Kit Starting Files/index.js
+++ 18_Drum Kit Starting Files/index.js
@@ -1,4 +1,10 @@
-document.querySelectorAll("button").addEventListener("click", clickHandled);
+var buttonList = document.getElementsByClassName('drum');
+
+for (i=0; i < buttonList.length; i++) {
+var currentButton = buttonList[i];
+currentButton.addEventListener('click',clickHandled)    
+} 
+
 function clickHandled () {
     alert ("I got clicked!"); 
 }
