eff9928cb772da3f33d6b9f9a3a4c18c2f6120a9
Justine <justine.ruysschaert@hotmail.com>
2024-05-07 16:16:41 +0200

    dfsd

--- 20_Simon/game.js
+++ 20_Simon/game.js
@@ -1,14 +1,9 @@
 var gamePattern =[];
 var buttonColors = ["red", "blue", "green", "yellow"];
-
-
-
-
 function nextSequence(){
     const randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
-
-    return randomNumber;
-
-}
+    $("#"+randomChosenColour).animate({opacity: 0})
+};
+nextSequence();
