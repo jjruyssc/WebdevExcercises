f04761142e0d7a28221e79ec58c6981fca6d9a9f
Justine <justine.ruysschaert@hotmail.com>
2024-05-16 18:47:24 +0200

    simon game stap 2 gedaan

--- 20_Simon/game.js
+++ 20_Simon/game.js
@@ -1,9 +1,15 @@
 var gamePattern =[];
 var buttonColors = ["red", "blue", "green", "yellow"];
+
 function nextSequence(){
+   
     const randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[randomNumber];
+    var audio = new Audio('sounds/'+ randomChosenColour + '.mp3');
     gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).animate({opacity: 0})
+    $("."+randomChosenColour).animate({opacity: 0},500);
+    $("."+randomChosenColour).animate({opacity: 1},500);
+    audio.play();
+
 };
-nextSequence();
+window.nextSequence();
