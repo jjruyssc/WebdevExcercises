5734e96ab4ad09a13c2ff2db1ca72314aaf0bec0
Justine <justine.ruysschaert@hotmail.com>
2024-05-30 18:41:12 +0200

    adde sounds + click function

--- 20_Simon/game.js
+++ 20_Simon/game.js
@@ -1,15 +1,27 @@
 var gamePattern =[];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+
+$(".btn").click(function(){
+    
+    var userChosenColour = $(this).attr("id");
+    console.log('í was clicked' + userChosenColour);
+
+
+
+})
+
+
 function nextSequence(){
    
     const randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[randomNumber];
-    var audio = new Audio('sounds/'+ randomChosenColour + '.mp3');
+    var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
     gamePattern.push(randomChosenColour);
     $("."+randomChosenColour).animate({opacity: 0},500);
     $("."+randomChosenColour).animate({opacity: 1},500);
-    audio.play();
+    return audio.play();
 
 };
+
 window.nextSequence();
