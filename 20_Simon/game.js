3664f2a5075a471cce98ce3a8559cf12c540e4f1
Justine <justine.ruysschaert@hotmail.com>
2024-06-24 17:59:54 +0200

    toevoeging onderdelen

--- 20_Simon/game.js
+++ 20_Simon/game.js
@@ -1,13 +1,17 @@
 var gamePattern =[];
+var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 
 $(".btn").click(function(){
     
     var userChosenColour = $(this).attr("id");
+    playSound(userChosenColour);
+    gamePattern.push(userChosenColour);
     console.log('í was clicked' + userChosenColour);
-
-
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    animatePress(userChosenColour);
 
 })
 
@@ -16,12 +20,20 @@ function nextSequence(){
    
     const randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[randomNumber];
-    var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
-    gamePattern.push(randomChosenColour);
+    playSound(randomChosenColour);
     $("."+randomChosenColour).animate({opacity: 0},500);
     $("."+randomChosenColour).animate({opacity: 1},500);
-    return audio.play();
-
 };
+
+function playSound(name){
+    var audio = new Audio('sounds/' + name + '.mp3')
+    audio.preload = 'auto';
+    return audio.play();
+}
+
+function animatePress(currentColour){
+    $("."+currentColour).toggleClass("pressed");
+}
+
 
 window.nextSequence();
